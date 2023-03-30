4834928ce56587418fe59ff6911ddc57005e8330
Zac <zbenattar@gmail.com>
2023-03-30 19:41:30 +0100

    Started on serializers and views

--- mysite/api/serializers.py
+++ mysite/api/serializers.py
@@ -1,32 +1,13 @@
-#converts python model objects to json inkedId
+# converts python model objects to json inkedId
 from rest_framework.serializers import ModelSerializer, IntegerField, CharField
-from projects.models import * 
+from projects.models import *
 
-class ProjectSerializer(ModelSerializer):
-    completion = IntegerField(source='get_completion')
-    start_date_unix = IntegerField(source='get_start_date_unix')
-    initial_deadline_unix = IntegerField(source='get_initial_deadline_unix')
-    current_deadline_unix = IntegerField(source='get_current_deadline_unix')
-    class Meta:
-        model = Project
-        exclude = ('start_date', 'initial_deadline', 'current_deadline')
-        read_only_fields = ('completion', 'start_date_unix', 'initial_deadline_unix', 'current_deadline_unix')
-
-class RoleSerializer(ModelSerializer):
-    class Meta:
-        model = Role
-        fields = '__all__'
-
-class RoleRequirementSerializer(ModelSerializer):
-    class Meta:
-        model = RoleRequirement
-        fields = '__all__'
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
-        fields ='__all__'
-        
+        fields = '__all__'
+
     def create(self, validated_data):
         password = validated_data.pop('password')
         user = super().create(validated_data)
@@ -34,58 +15,58 @@ class UserSerializer(ModelSerializer):
         user.save()
         return user
 
-class SkillSerializer(ModelSerializer):
+
+class PostSerializer(ModelSerializer):
     class Meta:
-        model = Skill
+        model = Post
         fields = '__all__'
 
+
+class AlbumPostSerializer(ModelSerializer):
+    class Meta:
+        model = AlbumPost
+        fields = '__all__'
+
+
+class BlogPostSerializer(ModelSerializer):
+    class Meta:
+        model = BlogPost
+        fields = '__all__'
+
+
+class VideoPostSerializer(ModelSerializer):
+    class Meta:
+        model = VideoPost
+        fields = '__all__'
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class TagSerializer(ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+    # Not sure if this is relevant
     def create(self, validated_data):
         get_user = self.context['request'].user
-        skill = super().create(validated_data)
-        get_user.skillset.add(skill)
+        tag = super().create(validated_data)
+        get_user.interests.add(tag)
         get_user.save()
-        return skill
+        return tag
 
-class MemberSerializer(ModelSerializer):
-    class Meta:
-        model = Member
-        fields = '__all__'
+# class TaskSerializer(ModelSerializer):
+#     creation_date_unix = IntegerField(source='creation_date_to_unix')
+#     start_date_unix = IntegerField(source='start_date_to_unix')
+#     earliest_finish_date_unix = IntegerField(source='earliest_finish_date_to_unix')
+#     latest_finish_date_unix = IntegerField(source='latest_finish_date_to_unix')
+#     dependent_tasks_string = CharField(source='get_dependent_tasks_string')
 
-class TimeWorkedSerializer(ModelSerializer):
-    class Meta:
-        model = TimeWorked
-        fields = '__all__'
-
-class RiskEvaluationSerializer(ModelSerializer):
-    date_unix = IntegerField(source='get_date_unix')
-    class Meta:
-        model = RiskEvaluation
-        read_only_fields = ('date_unix',)
-        exclude = ('date',)
-
-class FeedbackSerializer(ModelSerializer):
-    class Meta:
-        model = Feedback
-        fields = '__all__'
-
-class SuggestionSerializer(ModelSerializer):
-    class Meta:
-        model = Suggestion
-        fields = '__all__'
-
-class MeetingSerializer(ModelSerializer):
-    class Meta:
-        model = Meeting
-        fields = '__all__'
-        
-class TaskSerializer(ModelSerializer):
-    creation_date_unix = IntegerField(source='creation_date_to_unix')
-    start_date_unix = IntegerField(source='start_date_to_unix')    
-    earliest_finish_date_unix = IntegerField(source='earliest_finish_date_to_unix')
-    latest_finish_date_unix = IntegerField(source='latest_finish_date_to_unix')
-    dependent_tasks_string = CharField(source='get_dependent_tasks_string')
-    
-    class Meta:
-        model = Task
-        exclude = ('creation_date', 'start_date', 'latest_finish')
-        read_only_fields = ('creation_date_unix', 'start_date_unix', 'earliest_finish_date_unix', 'latest_finish_date_unix', 'dependent_tasks_string')
+#     class Meta:
+#         model = Task
+#         exclude = ('creation_date', 'start_date', 'latest_finish')
+#         read_only_fields = ('creation_date_unix', 'start_date_unix', 'earliest_finish_date_unix', 'latest_finish_date_unix', 'dependent_tasks_string')
